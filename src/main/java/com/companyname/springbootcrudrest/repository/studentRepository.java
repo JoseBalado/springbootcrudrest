1a8fcd9defb9ff837c60458dd690cd9ee9387917
Jose Balado <JoseBalado@users.noreply.github.com>
2019-09-10 14:03:19 +0100

    Create studentRepository interface

--- src/main/java/com/companyname/springbootcrudrest/repository/studentRepository.java
+++ src/main/java/com/companyname/springbootcrudrest/repository/studentRepository.java
@@ -3,8 +3,8 @@ package com.companyname.springbootcrudrest.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.companyname.springbootcrudrest.model.User;
+import com.companyname.springbootcrudrest.model.Student;;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long>{
+public interface studentRepository extends JpaRepository<Student, Long>{
 }
